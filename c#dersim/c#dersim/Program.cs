db85d0a3781014cf695c5739c6c0d4900f6728c5
hasanberkkarabulut <hasanberkarabulut@gmail.com>
2024-09-29 20:15:21 +0300

    Add files via upload

---
+++ c#dersim/c#dersim/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_dersim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // c# kodlarını yazacağımız yer
+            Console.Write("Merhaba");
+            Console.WriteLine("Merhaba2");
+            Console.ReadLine();
+        }
+    }
+}
